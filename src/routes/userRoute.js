e1b07d00d4d674edd8fd65d1b35f91e5e4ecc2b8
Matheus Fujihara <fujihara_matheus@hotmail.com>
2021-02-22 15:09:14 -0300

    removing authorization

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -6,7 +6,8 @@ const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth.json');
 
 // Utiliza o middleware para validar as requisicoes nas rotas abaixo
-router.use(authMiddleware);
+
+// router.use(authMiddleware);
 
 // Funcionalidades do tipo GET, PUT, DELETE
 // So podem ser utilizado pelo admin ou pelo propio usuario
@@ -14,10 +15,10 @@ router.use(authMiddleware);
 const userRoute = {
     get: async (req, res) => {
         try {
-            const decoded = decodedToken(req.headers.authorization);
+            // const decoded = decodedToken(req.headers.authorization);
 
-            if (decoded.email !== "admin@admin.com")
-                res.status(401).send({ error: 'Unauthorized! Its necessary to be an administrator.'});
+            // if (decoded.email !== "admin@admin.com")
+            //     res.status(401).send({ error: 'Unauthorized! Its necessary to be an administrator.'});
 
             const users = await User.find();
     
@@ -30,19 +31,19 @@ const userRoute = {
         try {
             const { name, email} = req.body;
             const { userEmail } = req.params;
-            const user = await User.find({ email: userEmail });
-            const decoded = decodedToken(req.headers.authorization);
+            // const user = await User.find({ email: userEmail });
+            // const decoded = decodedToken(req.headers.authorization);
 
-            if (decoded.email !== user.email) {
-                if (decoded.email == "admin@admin.com") {
-                    await User.findByIdAndUpdate({ email: userEmail }, { 
-                        name, 
-                        email
-                    });
-                    res.send({ user });
-                }           
-                res.status(401).send({ error: 'Unauthorized! Its necessary to be an administrator.'});                    
-            }
+            // if (decoded.email !== user.email) {
+            //     if (decoded.email == "admin@admin.com") {
+            //         await User.findByIdAndUpdate({ email: userEmail }, { 
+            //             name, 
+            //             email
+            //         });
+            //         res.send({ user });
+            //     }           
+            //     res.status(401).send({ error: 'Unauthorized! Its necessary to be an administrator.'});                    
+            // }
 
             user = await User.findOneAndUpdate({ email: userEmail }, { 
                 name, 
@@ -61,17 +62,17 @@ const userRoute = {
     delete: async (req, res) => {
         try {
             const { userEmail } = req.params;
-            const user = await User.find({ email: userEmail });
-            const decoded = decodedToken(req.headers.authorization);
+            // const user = await User.find({ email: userEmail });
+            // const decoded = decodedToken(req.headers.authorization);
 
-            if (decoded.email !== user.email) {
-                if (decoded.email == "admin@admin.com") {
-                    await User.findByIdAndRemove({ email: userEmail });
-                    return res.status(200).send({ success: true, response: 'Successfully delete'});
-                }
+            // if (decoded.email !== user.email) {
+            //     if (decoded.email == "admin@admin.com") {
+            //         await User.findByIdAndRemove({ email: userEmail });
+            //         return res.status(200).send({ success: true, response: 'Successfully delete'});
+            //     }
 
-                res.status(401).send({ error: 'Unauthorized! Its necessary to be an administrator.'});                    
-            }
+            //     res.status(401).send({ error: 'Unauthorized! Its necessary to be an administrator.'});                    
+            // }
 
             await User.findOneAndRemove({ email: userEmail });
     
@@ -82,9 +83,9 @@ const userRoute = {
     },
     getUser: async (req, res) => {
         try {
-            const decoded = decodedToken(req.headers.authorization);
-            if (decoded.email !== "admin@admin.com")
-                res.status(401).send({ error: 'Unauthorized! Its necessary to be an administrator.'});
+            // const decoded = decodedToken(req.headers.authorization);
+            // if (decoded.email !== "admin@admin.com")
+            //     res.status(401).send({ error: 'Unauthorized! Its necessary to be an administrator.'});
 
             const user = await User.find({ email: userEmail });
     
